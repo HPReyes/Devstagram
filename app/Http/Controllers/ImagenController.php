9fde0d52c6b1b73dbb24d07db55e87de03de8f0e
HPReyes <hpreyesdev@gmail.com>
2024-03-10 19:49:32 +0100

    intervention image2

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -1,35 +1,26 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
- 
-use Intervention\Image\ImageManager;
-use Intervention\Image\Drivers\Imagick\Driver;
- 
+use Intervention\Image\Facades\Image;
+
 class ImagenController extends Controller
 {
     //
     public function store(Request $request)
     {
         $imagen = $request->file('file');
- 
+
         $nombreImagen = Str::uuid() . "." . $imagen->extension();
- 
-        $manager = new ImageManager(new Driver());
-        $imagenServidor = $manager::imagick()->read($imagen);
-        $imagenServidor->resize(500, 500);
- 
+
+        $imagenServidor = Image::make($imagen);
+        $imagenServidor->fit(1000, 1000);
+
         $imagenPath = public_path('uploads') . '/' . $nombreImagen;
-        
         $imagenServidor->save($imagenPath);
- 
-        return response()->json([
-            'imagen' => $nombreImagen,
-        ]);
+
+        return response()->json(['imagen' => $nombreImagen ]);
     }
-} 
-
-
-
+}
